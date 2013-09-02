1ffaa240080f12a046a970bd231e8c796ed3b2eb
johli603 <johli603@student.liu.se>
2013-09-02 12:42:26 +0200

    Updated skeleton code per jonatan's tips. Added useful variables, removed the class

--- exc1_skelCode.py
+++ exc1_skelCode.py
@@ -1,146 +1,140 @@
 #
-# This is the file stub.py that can be used as a starting point for the bots
+# This file can be used as a starting point for the bot in exercise 1
 #
-
-import libpyAI as ai
 
 import sys
 import traceback
 import math
 import os
-
+import libpyAI as ai
 from optparse import OptionParser
 
-parser = OptionParser()
-
-parser.add_option ("-g", "--group", action="store", type="int", 
-                   dest="group", default=0, 
-                   help="The group number. Used to avoid port collisions when" 
-                   " connecting to the server.")
 
 #
-# Create a class used to store the internal state of the bot
+# Create variables that persist between ticks.
 #
 
-class myai:
-    def __init__(self):
-        self.count = 0
-        self.mode = "wait"
-        self.targetId = -1
+tickCount = 0
+mode = "wait"
+targetId = -1
 
-    def tick(self):
-        try:
+def tick(self):
+    #
+    # The API won't print out exceptions, so we have to catch and print them ourselves
+    #
+    try:
 
-            #
-            # If we die then restart the state machine in the state "aim"
-            #
-            if not ai.selfAlive():
-                self.count = 0
-                self.mode = "aim"
-                return
+        #
+        # If we die then restart the state machine in the "aim" state
+        #
+        if not ai.selfAlive():
+            self.count = 0
+            self.mode = "aim"
+            return
 
-            self.count += 1
+        self.count += 1
 
-            #
-            # Read the "sensors"
-            #
-            x = ai.selfX()
-            y = ai.selfY()
-            heading = ai.selfHeadingRad() 
-            # 0-2pi, 0 in x direction, positive toward y
+        #
+        # Read some "sensors" into local variable to avoid excessive calls to the API.
+        #
+        selfX = ai.selfX()
+        selfY = ai.selfY()
+        selfHeading = ai.selfHeadingRad() 
+        # 0-2pi, 0 in x direction, positive toward y
 
-            targetCount = ai.targetCountServer()
-            targetCountAlive = 0
+        targetCount = ai.targetCountServer()
+        targetCountAlive = 0
 
-            for i in range(targetCount):
-              if ai.targetAlive(i):
-                targetCountAlive += 1
+        for i in range(targetCount):
+          if ai.targetAlive(i):
+            targetCountAlive += 1
 
-            # For debugging use print statements, either here or further down in the code.
-            # useful functions: round(), ai.radToDeg, ai.degToRad, etc.
-            # os.system('clear') clears the terminal screen, which can be useful.
+        # Use print statements for debugging, either here or further down in the code.
+        # useful functions: round(), ai.radToDeg, ai.degToRad, etc.
+        # os.system('clear') clears the terminal screen, which can be useful.
 
-            print(self.count, self.mode, round(ai.radToDeg(heading)), targetCountAlive)
-
-
-            if self.mode == "wait":
-              if numTargetsAlive > 0:
-                self.mode = "aim"
-
-            elif self.mode == "aim":
-              if numTargetsAlive == 0:
-                self.mode = "wait"
-                return
-
-              # Find a target that is alive and save it's index in self.targetId.
-              # useful function ai.targetAlive
-
-              """your code here"""
-
-              # Calculate the direction the target is in
-              # useful functions: math.atan2, ai.targetX, ai.targetY
-
-              """your code here"""
-
-              # Turn to the direction of the target
-              # useful functions: turnRad, turnToRad
-
-              """your code here"""
-
-              # If the ship keeps oscillating between a few angles
-              # it may be due to latency. Only turning every second
-              # or third tick is a simple solution (use self.count and %)
+        print("tick count:", tickCount, "mode:", mode, "heading:",
+                round(ai.radToDeg(heading)), "targets alive:", targetCountAlive)
 
 
-              # If you are finished aiming change mode to shoot
-              # useful functions: ai.angleDiffRad, ai.angleDiffDeg, ai.radToDeg, ai.degToRad
+        if self.mode == "wait":
+          if numTargetsAlive > 0:
+            self.mode = "aim"
 
-              """your code here"""
+        elif self.mode == "aim":
+          if numTargetsAlive == 0:
+            self.mode = "wait"
+            return
 
-            elif self.mode == "shoot":
+          # Find a target that is alive and save the index in targetId.
+          # useful variables: targetCount, targetId
+          # useful functions: ai.targetAlive
 
-              # Shoot the target
-              # useful functions: ai.fireShot
+          """your code here"""
 
-              """your code here"""
+          # Calculate what direction the target is in and save in
+          # the variable targetDirection
+          # useful variables: selfX, selfY
+          # useful functions: math.atan2, ai.targetX, ai.targetY
 
-              # if the target is destroyed, change state
-              # useful functions: ai.targetAlive
+          """your code here"""
 
-              """your code here"""
+          # Turn to the direction of the target
+          # useful variables: targetDirection
+          # useful functions: turnRad, turnToRad
 
-        except:
-            print(traceback.print_exc())
-            print(sys.exc_info())
+          """your code here"""
 
-#
-# Create an instace of the bot class myai.
-#
+          # If the ship keeps oscillating between a few angles
+          # it may be due to latency. Only turning every second
+          # or third tick is a simple solution (use self.count and %)
 
-bot = myai()
 
-#
-# Connect the bot instance with the AI loop
-#
+          # If you are finished aiming change mode to shoot
+          # useful variables: selfHeading, targetDirection, mode
+          # useful functions: ai.angleDiffRad, ai.angleDiffDeg, ai.radToDeg, ai.degToRad
 
-def AI_loop():
-    bot.tick()
+          """your code here"""
+
+        elif self.mode == "shoot":
+
+          # Shoot the target
+          # useful functions: ai.fireShot
+
+          """your code here"""
+
+          # if the target is destroyed, change state to aim
+          # useful variables: targetId, mode
+          # useful functions: ai.targetAlive
+
+          """your code here"""
+
+    except:
+        print(traceback.print_exc())
+        print(sys.exc_info())
+
 
 #
 # Parse the command line arguments
 #
+parser = OptionParser()
+
+parser.add_option ("-p", "-port", action="store", type="int", 
+                   dest="port", default=15345, 
+                   help="The port to use. Used to avoid port collisions when" 
+                   " connecting to the server.")
 
 (options, args) = parser.parse_args()
 
-port = 15345 + options.group
+port = options.port
 name = "Exc. 1 skeleton" #Feel free to change this
 
 #
-# Start the main loop. Callback are done to AI_loop.
+# Start the main loop. Callback are done to tick.
 #
 
-ai.start(AI_loop,["-name", name, 
-                  "-join", 
-                  "-turnSpeed", "64",
-                  "-turnResistance", "0",
-                  "-port", str(port)])
+ai.start(tick, ["-name", name, 
+                "-join", 
+                "-turnSpeed", "64",
+                "-turnResistance", "0",
+                "-port", str(port)])
